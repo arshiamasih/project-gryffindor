8ea8fc4e5217dd8f531e4384218560892b673c1a
justnoah <noahxman@gmail.com>
2019-01-19 21:38:48 -0800

    getValue function; main.js.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function getValue () {
+  var name = document.querySelector('#name').value;
+  var last = document.querySelector('#last').value;
+  var pref = document.querySelector('#pref').value;
+}
