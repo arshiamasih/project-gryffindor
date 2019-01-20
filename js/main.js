f13b7158a3d14880401fb5d49a178cbfeda67c7e
justnoah <noahxman@gmail.com>
2019-01-19 21:06:09 -0800

    Played with forms; main.js.

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function getValue() {
-  var name = document.getElementById("#name");
-  var last = document.getElementById("#last");
-  var pref = document.getElementById("#pref");
-  return `${name} ate ${last}, and would like ${pref}`;
-}
-
-
-console.log(getValue());
